a1e3fb17af4155bf062fe6453385e0cfd9a46e09
Heandy27 <andy_heredia2008@hotmail.com>
2025-04-24 16:20:23 -0500

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Andy Heredia on 24/4/25.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
